c8ad4e865f6a70e7ad905dd8d46e0c989353f5f4
Abhishek Shenoy <abhishekshenoy@hotmail.co.uk>
2023-08-25 04:40:29 +0100

    Add dist and update gitignore

---
+++ dist/install.ps1
@@ -0,0 +1,80 @@
+
+param (
+    [string]$ServerUrl,
+    [string]$PluginsPath
+)
+
+
+$baseUrl = $ServerUrl
+$pluginsSavePath = $PluginsPath
+
+
+# $addinFolderPath = Join-Path -Path $pluginsSavePath -ChildPath 'temp'
+# $manifestSavePath = Join-Path -Path $addinFolderPath -ChildPath 'manifest.xml'
+# $regSavePath = Join-Path -Path $addinFolderPath -ChildPath 'plugin.reg'
+
+
+$addinFolderPath = $pluginsSavePath + '\\temp'
+$manifestSavePath = $addinFolderPath + '\\manifest.xml'
+$regSavePath = $addinFolderPath + '\\plugin.reg'
+
+$loggingPath = $pluginsSavePath + '\\OfficeAddins.log.txt'
+
+
+# -------------------
+# Create the addinFolder if it doesn't exist
+if (-not (Test-Path -Path $addinFolderPath)) {
+    New-Item -Path $addinFolderPath -ItemType Directory -Force
+}
+# -------------------
+
+
+
+
+
+$manifestUrl = $baseUrl + '/manifest.xml'
+$regUrl = $baseUrl + '/template.reg'
+
+
+# -------------------
+
+Invoke-WebRequest -Uri $manifestUrl -OutFile $manifestSavePath
+Invoke-WebRequest -Uri $regUrl -OutFile $regSavePath
+
+(Get-Content $regSavePath) -replace '{MANIFEST_PATH}', $manifestSavePath | Set-Content $regSavePath
+
+
+
+# -------------------
+
+$xml = [xml](Get-Content $manifestSavePath)
+
+$addinName = $xml.OfficeApp.DisplayName.DefaultValue
+$kebabCaseAddinName = $addinName -replace '\s', '-' -replace '[^-a-z0-9]', ''
+Write-Host "App Name: $kebabCaseAddinName"
+
+$addinId = $xml.OfficeApp.Id
+Write-Host "Add-in ID: $addinId"
+
+
+# -------------------
+
+
+
+(Get-Content $regSavePath) -replace '{ADDIN_ID}', $addinId | Set-Content $regSavePath
+(Get-Content $regSavePath) -replace '{LOGGING_PATH}', $loggingPath | Set-Content $regSavePath
+
+
+# -------------------
+
+
+Invoke-Command { reg import $regSavePath }
+Write-Host "Installed plugin"
+
+
+# -------------------
+
+
+Remove-Item -Path $regSavePath
+
+Rename-Item -Path $addinFolderPath -NewName $kebabCaseAddinName
