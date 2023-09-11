ab1b7b4ca7bbd449bead86a972b2d439da670257
Abhishek Shenoy <abhishekshenoy@hotmail.co.uk>
2023-09-11 23:03:43 +0100

    Add JSON fetch and parse functions

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -35,6 +35,89 @@ export function subscribe(channel, datakey, invocation) {
 
 
 
+/**
+  * Fetch JSON from URL
+  * @customfunction 
+  * @param {string} url string name of Github user or organization.
+  * @return {string} JSON response string
+  */
+export async function fetchJson(url) {
+    try {
+        const response = await fetch(url);
+        console.log(response)
+        if (!response.ok) {
+            console.log(response.status)
+            // throw new Error(response.statusText)
+        }
+        const jsonResponse = await response.json();
+        const jsonString = JSON.stringify(jsonResponse);
+        console.log(jsonString);
+        return jsonString;
+    }
+    catch (error) {
+        return error;
+    }
+}
+
+
+/**
+  * Parse JSON from JSON string
+  * @customfunction 
+  * @param {string} json JSON string to parse
+  * @param {string} key
+  * @return {string} JSON response string
+  */
+export function extract(json, key) {
+    const keys = key.split('.');
+    let value = JSON.parse(json);
+    console.log(value)
+
+    for (const key of keys) {
+        if (value.hasOwnProperty(key)) {
+            value = value[key];
+            console.log(value)
+        } else {
+            return undefined;
+        }
+    }
+
+    return JSON.stringify(value);
+}
+
+
+
+/**
+  * Convert JSON string to Excel range
+  * @customfunction 
+  * @param {string} json JSON string to parse
+  * @param {boolean} includeKeys Include object keys as headers
+  * @return {string[][]} Dynamic range array with multiple results
+  */
+export function parseJsonRange(json, includeKeys=false) {
+    const obj = JSON.parse(json)
+    var list = obj;
+    if (!Array.isArray(obj) || obj.length === 0) {
+        // Handle cases where the input is not a list of objects
+        list = [obj];
+    }
+
+    const result = [];
+
+    const keys = Object.keys(list[0]);
+    if (includeKeys) {
+        result.push(keys);
+    }
+
+    for (const obj of list) {
+        const values = keys.map(key => obj[key]);
+        result.push(values);
+    }
+    return result;
+}
+
+
+
+
 
 
 /**
@@ -135,14 +218,13 @@ async function onBindingDataChanged(eventArgs) {
     await Excel.run(async (context) => {
         const binding = context.workbook.bindings.getItem(bindingId)
         const range = binding.getRange()
-        
+
         range.load(["address", "values"])
         await context.sync()
         // console.log(bindingId, range.address)
 
         const newValue = range.values[0][0]
-        if (newValue)
-        {
+        if (newValue) {
             window.histories[address].push(newValue)
         }
     })
@@ -161,11 +243,9 @@ async function onBindingDataChanged(eventArgs) {
  * @param {number} n The channel to subscribe to.
  * @param {CustomFunctions.StreamingInvocation<number[][]>} invocation Parameter to send results to Excel or respond to the user canceling the function. A dynamic array.
  */
-export async function History(address, n, invocation)
-{
+export async function History(address, n, invocation) {
     var newAddress = false
-    if (!(address in window.histories))
-    {
+    if (!(address in window.histories)) {
         window.histories[address] = []
         window.historyLengths[address] = n
         window.historyInvocations[address] = new Set()
@@ -175,10 +255,9 @@ export async function History(address, n, invocation)
     await Excel.run(async (context) => {
         const sheet = context.workbook.worksheets.getActiveWorksheet()
         const range = sheet.getRange(address)
-        
+
         // If new source address for stream, add a binding to listen for changes 
-        if (newAddress)
-        {
+        if (newAddress) {
             // Note: We have to use Excel.binding not Office.binding
             // https://learn.microsoft.com/en-us/javascript/api/excel/excel.binding?view=excel-js-preview
             // const bindingId = range.address // This looks like Sheet1!B3 instead of B3
